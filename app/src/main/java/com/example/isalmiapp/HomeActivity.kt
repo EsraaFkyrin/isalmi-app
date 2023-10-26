70bc74dc42c529e5098c84f412f88072af27a371
Esraa Fkyrin <mohsenesraa043@gmail.com>
2023-10-26 12:38:40 +0300

    +splash screen +home screen +font +colors +strings +home Tabs(quran,hadeth,tasbeh,radio) +Quran Fragment +assets

--- app/src/main/java/com/example/isalmiapp/HomeActivity.kt
+++ app/src/main/java/com/example/isalmiapp/HomeActivity.kt
@@ -2,10 +2,58 @@ package com.example.isalmiapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.example.isalmiapp.databinding.ActivityHomeBinding
+import com.example.isalmiapp.hadeth.HadethFragment
+import com.example.isalmiapp.radio.RadioFragment
+import com.example.isalmiapp.tasbeh.SebhaFragment
+import com.example.islamy_project.quran.QuranFragment
 
 class HomeActivity : AppCompatActivity() {
+    lateinit var viewBinding: ActivityHomeBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
+        viewBinding=ActivityHomeBinding.inflate(layoutInflater)
+        setContentView(viewBinding.root)
+        selectedItem()
+        viewBinding.contantView.bottomNavigation.selectedItemId=R.id.nav_quraan
+
+
+
     }
-}
+
+    private fun selectedItem() {
+        viewBinding.contantView.bottomNavigation.setOnItemSelectedListener {
+            when (it.itemId) {
+                R.id.nav_quraan -> {
+                    showfragment(QuranFragment())
+
+
+                }
+                R.id.nav_hadeth -> {
+                    showfragment(HadethFragment())
+                }
+
+                R.id.nav_sebha -> {
+                    showfragment(SebhaFragment())
+                }
+                R.id.nav_radio -> {
+                    showfragment(RadioFragment())
+                }
+
+            }
+            return@setOnItemSelectedListener true
+
+        }
+    }
+
+        private  fun showfragment(Fragment: Fragment){
+            supportFragmentManager
+                .beginTransaction()
+                //framelayout > the place show on the fragment
+                .replace(R.id.FrameLayout_fragment_container,Fragment)
+                .commit()
+
+
+        }
+    }
