15cbb07024c373127ee7c7fe49e1971f3ef799c9
Esraa Fkyrin <mohsenesraa043@gmail.com>
2023-10-30 09:05:36 +0200

    +Add sebha layout +sebha fragment +taseh titles

--- app/src/main/java/com/example/isalmiapp/tasbeh/SebhaFragment.kt
+++ app/src/main/java/com/example/isalmiapp/tasbeh/SebhaFragment.kt
@@ -5,19 +5,102 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
 import com.example.isalmiapp.R
+import com.example.isalmiapp.constant
 import com.example.isalmiapp.databinding.FragmentHadethBinding
+import com.example.isalmiapp.databinding.FragmentSebhaBinding
 
 
 class SebhaFragment : Fragment() {
-    lateinit var ViewBinding: FragmentHadethBinding
+
+    lateinit var img_view: ImageView
+    lateinit var txtview_convert: TextView
+    lateinit var txtview_counter: TextView
+    lateinit var viewBinding: FragmentSebhaBinding
+    var counter = 0
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_radio, container, false)
+        viewBinding = FragmentSebhaBinding.inflate(layoutInflater, container, false)
+        return viewBinding.root
     }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        txtview_convert = viewBinding.tvConvertSobhanAaalh
+        txtview_convert.text= constant.SOBHANALLAH
+        img_view = viewBinding.imgBodySebha
+        txtview_counter = viewBinding.TheNumber
+        txtview_counter.text = "$counter"
+
+
+
+        img_view.setOnClickListener(View.OnClickListener {
+            OnClickSebhaImage()
+
+        })
+    }
+
+
+    fun OnClickSebhaImage() {
+        img_view.rotation = img_view.rotation + 5
+
+        counter++
+        txtview_counter.text = "$counter"
+
+        if(txtview_convert.text==constant.ELKETMA)
+        {
+            txtview_convert.text = constant.SOBHANALLAH
+            counter = 0
+            txtview_counter.text = "$counter"
+        }
+
+        if (counter == 33) {
+            when (txtview_convert.text) {
+                constant.SOBHANALLAH -> {
+                    txtview_convert.text = constant.ELHAMDOLLAH
+                    counter = 0
+                    txtview_counter.text = "$counter"
+                }
+                constant.ELHAMDOLLAH -> {
+                    txtview_convert.text=constant.ALLAHAKBR
+                    counter = 0
+                    txtview_counter.text = "$counter"
+
+                }
+                constant.ALLAHAKBR -> {
+                    txtview_convert.text=constant.AISTIGHFAR
+                    counter = 0
+                    txtview_counter.text = "$counter"
+                }
+                constant.AISTIGHFAR -> {
+                    txtview_convert.text=constant.SALAH
+                    counter = 0
+                    txtview_counter.text = "$counter"
+                }
+                constant.SALAH -> {
+                    txtview_convert.text=constant.ELKETMA
+                    counter = 0
+                    txtview_counter.text = "$counter"
+                }
+                constant.ELKETMA -> {
+                    txtview_convert.text=constant.ELHAMDOLLAH
+                    counter = 0
+                    txtview_counter.text = "$counter"
+                }
+
+            }
+
+        }
+
+    }
+
+
+
+
 
 
                 }
